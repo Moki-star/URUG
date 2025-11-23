dba9460ba783fe3d5d7ecf8b5608dbaa3abf0b37
Moki-star <phonekyinaing28@gmail.com>
2025-11-23 17:22:04 +0630

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
